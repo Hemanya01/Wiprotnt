71be44fa73253beb229dd4a7be8bc2c9a2688954
Hemanya01 <hemanyavelivela01@gmail.com>
2026-07-07 22:21:05 +0530

    loopingconstructs commit

---
+++ wiprotnt/LogicBuilding/src/LoopingContructs/BackwardSkipping.java
@@ -0,0 +1,12 @@
+package LoopingContructs;
+
+public class BackwardSkipping {
+	public static void main(String[] args)
+	{
+		for(int i=100;i>=0;i=i-3)
+		{
+			System.out.print(i+" ");
+		}
+	}
+
+}
